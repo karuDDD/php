b6e1875a19de271af5bc5f4d0a5938137e215044
303B-09\cre <a>
2023-07-18 19:22:14 +0900

    課題進めた7/18

---
+++ kadai1/q2.php
@@ -0,0 +1,12 @@
+<!--問題 2（初級）:
+二つの変数 num1 と num2 があります。num1 が num2 より大きい場合は「num1 is greater than num2」、小さい場合は「num1 is less than num2」、等しい場合は「num1 is equal to num2」と表示する PHP プログラムを作成してください。 -->
+<?php
+echo "Hello World！\n";
+
+$num1=10;
+$num2=20;
+
+if($num1 > $num2) {
+    echo "num1 is greater than num2\n";
+}
+?>
