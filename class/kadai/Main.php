d41ea2408e8165f303d63ce8b60a8f1db15c6fa2
303B-09\cre <a>
2023-10-03 19:17:29 +0900

    10/3課題進めた

--- class/kadai/Main.php
+++ class/kadai/Main.php
@@ -3,14 +3,16 @@
 // Userクラスの読み込み
 require 'Gun.php';
 
-$mainWeapon = new Gun("AK47",30,0);
-$subWeapon = new Gun("Mosin-Nagant",5,0);
+$mainWeapon = new Gun("AK47",30,30);
+$subWeapon = new Gun("Mosin-Nagant",5,5);
 
 // 現在の状態を表示
 $mainWeapon->echoStatus();
 $subWeapon->echoStatus();
+$currentMagazine->echoStatus();
 
-$return->echoStatus();
+return $this->reload;
 
 $mainWeapon->echoStatus();
 $subWeapon->echoStatus();
+$currentMagazine->echoStatus();
